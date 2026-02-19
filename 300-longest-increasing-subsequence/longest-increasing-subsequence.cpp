28189efe2d14a17c25cc1ee3fa83cda14d836095
Aaryan Tripathi <100665724+Aaryan1311@users.noreply.github.com>
2026-02-19 21:52:35 +0530

    Time: 560 ms (15.88%) | Memory: 296.5 MB (20.73%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -3,11 +3,12 @@ public:
     int helper(int n, int idx, int pre, vector<int>& arr, vector<vector<int>>& dp){
         if(idx == n) return 0;
         if(dp[idx][pre+1] != -1) return dp[idx][pre+1];
-        int len = helper(n,idx+1,pre,arr,dp);
+        int pick = 0;
+        int not_pick = helper(n,idx+1,pre,arr,dp);
         if(pre == -1 || arr[pre] < arr[idx]){
-            len = max(len,1 + helper(n,idx+1,idx,arr,dp));
+            pick = 1 + helper(n,idx+1,idx,arr,dp);
         }
-        return dp[idx][pre+1] = len;
+        return dp[idx][pre+1] = max(pick,not_pick);
     }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
