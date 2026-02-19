5329813c10aec91cd23af39016ba57f76bbd8258
Aaryan Tripathi <100665724+Aaryan1311@users.noreply.github.com>
2026-02-19 23:57:00 +0530

    Time: 143 ms (39.63%) | Memory: 14.4 MB (48.52%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,29 +1,19 @@
 class Solution {
 public:
-    int helper(int n, int idx, int pre, vector<int>& arr, vector<vector<int>>& dp){
-        if(idx == n) return 0;
-        if(dp[idx][pre+1] != -1) return dp[idx][pre+1];
-        int pick = 0;
-        int not_pick = helper(n,idx+1,pre,arr,dp);
-        if(pre == -1 || arr[pre] < arr[idx]){
-            pick = 1 + helper(n,idx+1,idx,arr,dp);
-        }
-        return dp[idx][pre+1] = max(pick,not_pick);
-    }
-    int lengthOfLIS(vector<int>& arr) {
-        int n = arr.size();
-       vector<int> next(n+1,0), curr(n+1,0);
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> next(n+1,0),cur(n+1,0);
         for(int idx = n-1;idx>=0;idx--){
             for(int pre = idx-1;pre>=-1;pre--){
                 int pick = 0;
                 int not_pick = next[pre+1];
-                if(pre == -1 || arr[pre] < arr[idx]){
-                    pick = 1 + next[idx+1];
+                if(pre == -1 || nums[pre] < nums[idx]){
+                    pick = 1+next[idx+1];
                 }
-                curr[pre+1] = max(pick,not_pick);
+                cur[pre+1] = max(pick,not_pick);
             }
-            next = curr;
+            next = cur;
         }
-        return curr[0];
+        return cur[0];
     }
 };
