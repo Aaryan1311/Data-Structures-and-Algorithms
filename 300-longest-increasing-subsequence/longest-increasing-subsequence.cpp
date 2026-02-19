3b735384c93472a54eae266a987f9709308d0e22
Aaryan Tripathi <100665724+Aaryan1311@users.noreply.github.com>
2026-02-19 22:05:47 +0530

    Time: 274 ms (33.81%) | Memory: 296.5 MB (20.73%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -10,9 +10,19 @@ public:
         }
         return dp[idx][pre+1] = max(pick,not_pick);
     }
-    int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>> dp(n,vector<int> (n+1,-1));
-        return helper(n,0,-1,nums,dp);
+    int lengthOfLIS(vector<int>& arr) {
+        int n = arr.size();
+        vector<vector<int>> dp(n+1,vector<int> (n+1,0));
+        for(int idx = n-1;idx>=0;idx--){
+            for(int pre = idx-1;pre>=-1;pre--){
+                int pick = 0;
+                int not_pick = dp[idx+1][pre+1];
+                if(pre == -1 || arr[pre] < arr[idx]){
+                    pick = 1 + dp[idx+1][idx+1];
+                }
+                dp[idx][pre+1] = max(pick,not_pick);
+            }
+        }
+        return dp[0][-1+1];
     }
 };
