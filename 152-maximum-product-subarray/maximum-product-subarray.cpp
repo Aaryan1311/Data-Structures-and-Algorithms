0beabdde8116307b15b74ea2c3880b193fa75c99
Aaryan Tripathi <100665724+Aaryan1311@users.noreply.github.com>
2026-02-19 20:26:54 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (5.68%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -1,34 +1,16 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> pre(n), suf(n);
-        if(nums[0] != 0) pre[0] = nums[0];
-        else pre[0] = 1;
-        int mxp = nums[0];
-        for(int i = 1;i<n;i++){
-            if(nums[i] == 0){
-                pre[i] = 1;
-                mxp = max(mxp,0);
-            }
-            else {
-                pre[i] = pre[i-1]*nums[i];
-                mxp = max(mxp,pre[i]);
-            }
+        int pre = 1;
+        int suf = 1;
+        int ans = INT_MIN;
+        for(int i = 0;i<nums.size();i++){
+            if(pre == 0) pre = 1;
+            if(suf == 0) suf = 1;
+            pre = pre*nums[i];
+            suf = suf*nums[nums.size()-i-1];
+            ans = max(ans,max(pre,suf));
         }
-        if(nums[n-1] != 0) suf[n-1] = nums[n-1];
-        else suf[n-1] = 1;
-        int mxs = nums[n-1];
-        for(int i = n-2;i>=0;i--){
-             if(nums[i] == 0){
-                suf[i] = 1;
-                mxs = max(mxs,0);
-            }
-            else {
-                suf[i] = suf[i+1]*nums[i];
-                mxs = max(mxs,suf[i]);
-            }
-        }
-        return max(mxp,mxs);
+        return ans;
     }
 };
