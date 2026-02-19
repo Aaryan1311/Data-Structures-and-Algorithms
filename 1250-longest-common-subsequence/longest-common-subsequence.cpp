01e7e53152983a94be8fb0b6be3778244e61d45b
Aaryan Tripathi <100665724+Aaryan1311@users.noreply.github.com>
2026-02-20 02:36:23 +0530

    Time: 15 ms (92.44%) | Memory: 9.3 MB (90.19%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,32 +1,24 @@
 class Solution {
 public:
-    int helper(int idx1, int idx2, string s1, string s2, vector<vector<int>>& dp){
-        if(idx1 == s1.size() || idx2 == s2.size()) return 0;
-        if(dp[idx1][idx2] != -1) return dp[idx1][idx2];
-        int ans = 0;
-        if(s1[idx1] == s2[idx2]){
-            ans = 1+helper(idx1+1,idx2+1,s1,s2,dp);
-        }
-        else{
-            ans = max(helper(idx1+1,idx2,s1,s2,dp),helper(idx1,idx2+1,s1,s2,dp));
-        }
-        return dp[idx1][idx2] = ans;
-    }
     int longestCommonSubsequence(string s1, string s2) {
         int idx1 = 0,idx2 = 0;
-        vector<vector<int>> dp(s1.size()+1, vector<int> (s2.size()+1,0));
+        int n1 = s1.size();
+        int n2 = s2.size();
+        n1 = max(n1,n2);
+        vector<int> nx1(n1+1,0),cur(n1+1,0);
         for(int idx1 = s1.size()-1;idx1>=0; idx1--){
             for(int idx2 = s2.size()-1;idx2>=0; idx2--){
                 int ans = 0;
                 if(s1[idx1] == s2[idx2]){
-                    ans = 1+dp[idx1+1][idx2+1];
+                    ans = 1+nx1[idx2+1];
                 }
                 else{
-                    ans = max(dp[idx1+1][idx2],dp[idx1][idx2+1]);
+                    ans = max(nx1[idx2],cur[idx2+1]);
                 }
-                dp[idx1][idx2] = ans;
+                cur[idx2] = ans;
             }
+            nx1 = cur;
         }
-        return dp[0][0];
+        return cur[0];
     }
 };
