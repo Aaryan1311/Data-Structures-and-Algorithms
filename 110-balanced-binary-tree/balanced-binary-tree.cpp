6db3e91ae01e965ada8ecb16f942337a43f2f170
Aaryan Tripathi <100665724+Aaryan1311@users.noreply.github.com>
2026-02-22 13:12:55 +0530

    Time: 0 ms (100.00%) | Memory: 22.9 MB (82.20%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -1,30 +1,26 @@
 /**
  * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+    struct Tree{
+        int val;
+        Tree *left;
+        Tree *right;
+        Tree() : val(0), left(nullptr), right(nullptr) {};
+        Tree(int val) : val(val), left(nullptr), right(nullptr) {};
+        Tree(int val, Tree *left, Tree *right) : val(val), left(left), right(right) {};
+    };
  */
 class Solution {
 public:
-    int height(TreeNode* root){
-        if(root == nullptr) return 0;
-        if(root->left == nullptr && root->right == nullptr) return 1;
-        int left = height(root->left);
-        int right = height(root->right);
-        if(left == -1 || right == -1) return -1;
-        if(abs(left - right) >= 2) return -1;
-        return max(left+1,right+1);
-
+    int helper(TreeNode* node){
+       if(node == nullptr) return 0;
+       int left = helper(node->left);
+       if(left == -1) return -1;
+       int right = helper(node->right);
+       if(right == -1) return -1;
+       if(abs(left-right) > 1) return -1;
+       return max(left,right)+1;
     }
     bool isBalanced(TreeNode* root) {
-        if(root == nullptr) return true;
-        int h = height(root);
-        if(h == -1) return false;
-        return true;
+       return helper(root) != -1;
     }
 };
