14914e874dfd217bdc2de4916e469b045f6eafd3
Aaryan Tripathi <100665724+Aaryan1311@users.noreply.github.com>
2026-02-24 02:18:20 +0530

    Time: 104 ms (42.24%) | Memory: 103.3 MB (16.80%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
-    int helper(int idx, int buy, int fee, vector<int>& arr, vector<vector<int>>& dp){
-        if(idx == arr.size()) return 0;
-        if(dp[idx][buy] != -1) return dp[idx][buy];
-        int choose = 0;
-        int notchoose = helper(idx+1,buy,fee,arr,dp);
-        if(buy){
-            choose = helper(idx+1,0,fee,arr,dp) - arr[idx];
-        }
-        else{
-            choose = helper(idx+1,1,fee,arr,dp) + arr[idx] - fee;
-        }
-
-        return dp[idx][buy] = max(choose,notchoose);
-    }
     int maxProfit(vector<int>& prices, int fee) {
-        vector<vector<int>> dp(prices.size(), vector<int>(2,-1));
-        return helper(0,1,fee,prices,dp);
+        vector<vector<int>> dp(prices.size()+1, vector<int> (2,0));
+        for(int idx = prices.size()-1; idx >= 0; idx--){
+            for(int can_buy = 0; can_buy < 2; can_buy++){
+                if(can_buy == 1){
+                    dp[idx][can_buy] = max(dp[idx+1][0] - prices[idx], dp[idx+1][can_buy]);
+                }
+                else{
+                    dp[idx][can_buy] = max(dp[idx+1][1] + prices[idx] - fee, dp[idx+1][can_buy]);
+                }
+            }
+        }
+        return dp[0][1];
     }
 };
