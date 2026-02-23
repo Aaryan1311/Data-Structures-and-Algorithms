9ed0514bd277a3ec8b199835ae4fb93428766cc8
Aaryan Tripathi <100665724+Aaryan1311@users.noreply.github.com>
2026-02-23 21:03:02 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (98.84%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -7,8 +7,8 @@ public:
         for(int i = 0;i<nums.size();i++){
             if(pre == 0) pre = 1;
             if(suf == 0) suf = 1;
-            pre = pre*nums[i];
-            suf = suf*nums[nums.size()-i-1];
+            pre *= nums[i];
+            suf *= nums[nums.size()-i-1];
             ans = max(ans,max(pre,suf));
         }
         return ans;
