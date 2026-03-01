35ae906adc72c2058ec8d0f8966c04d2e5c94101
Aaryan Tripathi <100665724+Aaryan1311@users.noreply.github.com>
2026-03-01 23:37:06 +0530

    Time: 3 ms (74.16%) | Memory: 27.6 MB (18.57%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -12,22 +12,21 @@
 class Solution {
 public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        unordered_map<int,int> mp;
         int n = preorder.size();
-        unordered_map<int, int> mp;
-        for(int i = 0;i<inorder.size();i++){
+        for(int i = 0;i<n;i++){
             mp[inorder[i]] = i;
         }
-        return helper(preorder,inorder,0,n-1,0,n-1,mp);
+        return build(preorder,inorder,0,n-1,0,n-1,mp);
     }
-    TreeNode* helper(vector<int>& preorder, vector<int>& inorder, int pres, int pree, int ins, int ine, unordered_map<int,int>& mp){
-        if(pres > pree || ins > ine) return nullptr;
-        TreeNode* node = new TreeNode(preorder[pres]);
-        int inroot_idx = mp[preorder[pres]];
-        int left_node = inroot_idx - ins;
-        TreeNode* left = helper(preorder, inorder, pres+1, pres+left_node, ins, inroot_idx-1, mp);
-        TreeNode* right = helper(preorder, inorder, pres+left_node+1,pree,inroot_idx+1,ine,mp);
-        node->left = left;
-        node->right = right;
+
+    TreeNode* build(vector<int>& preorder, vector<int>& inorder, int ps, int pe, int is, int ie, unordered_map<int,int>& mp){
+        if(ps > pe || is > ie) return nullptr;
+        TreeNode* node = new TreeNode(preorder[ps]);
+        int in_root = mp[preorder[ps]];
+        int left_node = in_root - is;
+        node->left = build(preorder,inorder,ps+1,ps+left_node,is,in_root-1,mp);
+        node->right = build(preorder,inorder,ps+left_node+1,pe,in_root+1,ie,mp);
         return node;
     }
 };
