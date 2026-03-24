78865fd1699481e2c2208786cb2da091d46e0e54
Aaryan Tripathi <100665724+Aaryan1311@users.noreply.github.com>
2026-03-24 21:28:31 +0530

    Time: 14 ms (20.48%) | Memory: 31.5 MB (83.06%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -1,59 +1,72 @@
 class Solution {
 public:
     int numEnclaves(vector<vector<int>>& grid) {
-        vector<vector<int>> vis(grid.size(), vector<int> (grid[0].size(),-1));
+        int row = grid.size();
+        int col = grid[0].size();
+        vector<vector<int>> vis(row, vector<int> (col,0));
         queue<pair<int,int>> q;
-        for(int i = 0;i<grid.size();i++){
+        for(int i = 0;i<row;i++){
             if(grid[i][0] == 1){
                 q.push({i,0});
                 vis[i][0] = 1;
+                grid[i][0] = -1;
             }
-            if(grid[i][grid[0].size()-1] == 1){
-                q.push({i,grid[0].size()-1});
-                vis[i][grid[0].size()-1] = 1;
+        }
+        for(int i = 0;i<row;i++){
+            if(grid[i][col-1] == 1){
+                q.push({i,col-1});
+                vis[i][col-1] = 1;
+                grid[i][col-1] = -1;
             }
-        }   
-        for(int j = 0;j<grid[0].size();j++){
+        }
+        for(int j = 0;j<col;j++){
             if(grid[0][j] == 1){
                 q.push({0,j});
                 vis[0][j] = 1;
-            }
-            if(grid[grid.size()-1][j] == 1){
-                q.push({grid.size()-1,j});
-                vis[grid.size()-1][j] = 1;
+                grid[0][j] = -1;
             }
         }
-
+        for(int j = 0;j<col;j++){
+            if(grid[row-1][j] == 1){
+                q.push({row-1,j});
+                vis[row-1][j] = 1;
+                grid[row-1][j] = -1;
+            }
+        }
         while(!q.empty()){
             pair<int,int> p = q.front();
             int i = p.first;
             int j = p.second;
-            //top 
-            if(i != 0 && grid[i-1][j] == 1 && vis[i-1][j] == -1){
-                q.push({i-1,j});
+
+            if(i > 0 && vis[i-1][j] == 0 && grid[i-1][j] == 1){
                 vis[i-1][j] = 1;
+                grid[i-1][j] = -1;
+                q.push({i-1,j});
             }
-            //down
-             if(i != grid.size()-1 && grid[i+1][j] == 1 && vis[i+1][j] == -1){
-                q.push({i+1,j});
-                vis[i+1][j] = 1;
-            }
-            //left
-             if(j != 0 && grid[i][j-1] == 1 && vis[i][j-1] == -1){
-                q.push({i,j-1});
+
+            if(j > 0 && vis[i][j-1] == 0 && grid[i][j-1] == 1){
                 vis[i][j-1] = 1;
+                grid[i][j-1] = -1;
+                q.push({i,j-1});
             }
-            //right
-             if(j != grid[0].size()-1 && grid[i][j+1] == 1 && vis[i][j+1] == -1){
-                q.push({i,j+1});
+
+            if(i < row-1 && vis[i+1][j] == 0 && grid[i+1][j] == 1){
+                vis[i+1][j] = 1;
+                grid[i+1][j] = -1;
+                q.push({i+1,j});
+            }
+
+            if(j < col-1 && vis[i][j+1] == 0 && grid[i][j+1] == 1){
                 vis[i][j+1] = 1;
+                grid[i][j+1] = -1;
+                q.push({i,j+1});
             }
             q.pop();
         }
         int cnt = 0;
-        for(int i = 0;i<grid.size();i++){
-            for(int j = 0;j<grid[0].size();j++){
-                if(grid[i][j] == 1 && vis[i][j] == -1) cnt++;
+        for(int i = 0;i<row;i++){
+            for(int j = 0;j<col;j++){
+                if(grid[i][j] == 1) cnt++;
             }
         }
         return cnt;
