423b8d8a1a9a268008fc23a8d0549fd930ffa577
Aaryan Tripathi <100665724+Aaryan1311@users.noreply.github.com>
2026-03-25 20:59:11 +0530

    Time: 3 ms (76.52%) | Memory: 19.2 MB (81.54%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    bool dfs(int idx,vector<int>& vis, vector<vector<int>>& adj){
+    bool dfs(int idx, vector<int>& vis, vector<vector<int>>& adj){
         vis[idx] = 1;
-        for(int i = 0;i<adj[idx].size();i++){
-            if(adj[idx][i] != -1 && vis[adj[idx][i]] == 1) return false;
-            else if(adj[idx][i] != -1 && vis[adj[idx][i]] == -1) {
-                if(!dfs(adj[idx][i],vis,adj)) return false;
+        for(int i : adj[idx]){
+            if(vis[i] == 1) return false;
+            else if(vis[i] == 0){
+                if(!dfs(i,vis,adj)) return false;
             }
         }
         vis[idx] = 2;
@@ -13,15 +13,13 @@ public:
     }
     bool canFinish(int n, vector<vector<int>>& arr) {
         vector<vector<int>> adj(n);
+        vector<int> vis(n,0);
         for(int i = 0;i<arr.size();i++){
             adj[arr[i][1]].push_back(arr[i][0]);
         }
-        vector<int> vis(n,-1);
         for(int i = 0;i<n;i++){
-            if(vis[i] == -1){
-                bool b = dfs(i,vis,adj);
-                if(!b) return false;
-            }
+            bool b = dfs(i,vis,adj);
+            if(!b) return false;
         }
         return true;
     }
