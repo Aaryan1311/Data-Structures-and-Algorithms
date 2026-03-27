59268c0af47f6149e8137a3a23c3315f325bdb06
Aaryan Tripathi <100665724+Aaryan1311@users.noreply.github.com>
2026-03-27 18:18:49 +0530

    Time: 51 ms (16.62%) | Memory: 36.7 MB (47.44%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    int helper(int idx,int sum, int tar, vector<int>& arr, vector<vector<int>>& dp){
+    int helper(int idx, int sum, int tar, vector<int>& arr, vector<vector<int>>& dp){
         if(sum == tar) return 1;
         if(idx == arr.size()) return 0;
         if(dp[idx][sum] != -1) return dp[idx][sum];
-
         int choose = 0;
-        int notchoose = helper(idx+1,sum,tar,arr,dp);
-        if(sum + arr[idx] <= tar) choose = helper(idx,sum+arr[idx],tar,arr,dp);
-
-        return dp[idx][sum] =  choose + notchoose;
+        int not_choose = helper(idx+1,sum,tar,arr,dp);
+        if(arr[idx] <= tar && arr[idx] + sum <= tar){
+            choose = helper(idx,sum+arr[idx],tar,arr,dp);
+        }
+        return dp[idx][sum] = choose + not_choose;
     }
     int change(int amount, vector<int>& coins) {
-        vector<vector<int>> dp(coins.size(), vector<int>(amount+1,-1));
+        vector<vector<int>> dp(coins.size(), vector<int> (amount+1,-1));
         return helper(0,0,amount,coins,dp);
     }
 };
