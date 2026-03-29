8e1ed35da4b35745496048796d724a081e1b5318
Aaryan Tripathi <100665724+Aaryan1311@users.noreply.github.com>
2026-03-29 11:11:39 +0530

    Time: 328 ms (26.22%) | Memory: 21.3 MB (32.60%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
-    int ladderLength(string b, string e, vector<string>& arr) {
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        set<string> words;
+        for(string s : wordList) words.insert(s);
         queue<pair<string,int>> q;
-        q.push({b,1});
-        vector<int> vis(arr.size(),-1);
+        q.push({beginWord,1});
+        words.erase(beginWord);
         while(!q.empty()){
             pair<string,int> p = q.front();
             string s = p.first;
-            int cnt = p.second;
-            for(int i = 0;i<arr.size();i++){
-                if(vis[i] == -1){
-                    // cout << arr[i] << endl;
-                    int diff = 0;
-                    for(int j = 0;j<arr[i].size();j++){
-                        if(arr[i][j] != s[j]) diff++;
-                    }
-                    if(diff == 1){
-                        vis[i] = 1;
-                        if(arr[i] == e) return cnt+1;
-                        cout << arr[i] << endl;
-                        q.push({arr[i],cnt+1});
-                    }
+            int step = p.second;
+            for(int i = 0;i<s.size();i++){
+                string temp = s;
+                for(int j = 0;j<26;j++){
+                    temp[i] = (char)('a'+j);
+                    if(words.find(temp) != words.end()){
+                        if(temp == endWord) return step+1;
+                        words.erase(temp);
+                        q.push({temp,step+1});
+                    } 
                 }
             }
             q.pop();
